bc750db525c3a940f39489f1de1c714b5d037528
Muffin-hash <kaleemmusa526@gmail.com>
2026-08-11 22:11:14 +0500

    Add files via upload

---
+++ word.dart
@@ -0,0 +1,13 @@
+class Word {
+  final String original;
+  final String translation;
+  final String category;
+  final String pronunciation;
+
+  Word({
+    required this.original,
+    required this.translation,
+    required this.category,
+    required this.pronunciation,
+  });
+}
